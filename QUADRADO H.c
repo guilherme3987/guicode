8e43261af04315421f9ccf776f152c898e878c02
Guilherme <76690766+guilherme3987@users.noreply.github.com>
2022-05-01 18:16:52 -0300

    Update QUADRADO H.c

--- QUADRADO H.c
+++ QUADRADO H.c
@@ -2,10 +2,14 @@
 #include <stdio.h>
 int main()
 {	
-    int teste,teste2,teste3,teste4,l,c;
-    int matriz[4][4] =  {1, 2, 3, 4, 2, 1, 4, 3, 3, 4, 1, 2, 4, 3, 2, 1};
+	// DECLARAÇÃO DAS VARIÁVEIS 	
+    int teste,teste2,teste3,teste4,l,c;              
+	// DECLARAÇÃO DA ARRAY(BIDIMENSIONAL)/MATRIZ
+    int matriz[4][4] =  {1, 2, 3, 4, 2, 1, 4, 3, 3, 4, 1, 2, 4, 3, 2, 1};  
     
-    	void percorrendo(int x[4][4],int teste);
+	
+	// RETORNO DAS FUNÇÕES 
+    	void percorrendo(int x[4][4],int teste); // FUNÇÕES QUE PERCORREM E TESTAM SE A MULTIPLICAÇÃO DOS ELEMENTOS DE LINHA E COLUNA SÃO IGUAIS 
 	percorrendo(matriz,teste);
 	
 	void percorrendo2(int x[4][4],int teste2);
@@ -17,11 +21,15 @@ int main()
 	void percorrendo4(int x[][4],int teste4);
 	percorrendo4(matriz,teste4);
 	
+	
+	// FUNÇÃO QUE TESTA SE É QUADRADO HOLÂNDES 
 	void seEHholandes(int teste,int teste2,int teste3,int teste4);
 	seEHholandes(teste,teste2,teste3,teste4);
 	
 
 }
+
+// FUNÇÕES QUE PERCORREM CADA LINHA E COLUNA 
 
 void percorrendo (int x[][4],int teste)
 {
@@ -35,9 +43,9 @@ void percorrendo (int x[][4],int teste)
 	
     for ( l=0;l<1;++l){
         for (c=0;c<4;++c){
-//            printf("%d,",x[l][c]);
+//            printf("%d,",x[l][c]);	// PRIMEIRA LINHA/COLUNA E GUARDA O RESULTADO
         }
-        teste = x[0][0] * x[0][1] * x[0][2] * x[0][3];
+        teste = x[0][0] * x[0][1] * x[0][2] * x[0][3];  
         printf("\n%d", teste);
     }
 }    
@@ -47,9 +55,9 @@ void percorrendo2(int x[][4],int teste2)
     int l,c;
     for (l=0;l<2;++l){
         for(c=0;c<4;++c){
-//           printf("%d  ",x[l][c]);
+//           printf("%d  ",x[l][c]);	// SEGUNDA LINHA/COLUNA E GUARDA O RESULTADO
         }
-        teste2 =  x[1][0] * x[1][1] * x[1][2] * x[1][3] ;
+        teste2 =  x[1][0] * x[1][1] * x[1][2] * x[1][3] ; 
        printf("\n\n%d"  ,teste2);
     }
     
@@ -60,7 +68,7 @@ void percorrendo3(int x[][4],int teste3)
     int l,c;
     for (l=0;l<3;++l){
         for(c=0;c<4;++c){
-//          printf("%d  ",x[l][c]);
+//          printf("%d  ",x[l][c]);	// TERCEIRA LINHA/COLUNA E GUARDA O RESULTADO
         }
         teste3 = x[2][0] * x[2][1] * x[2][2] * x[2][3] ;
         printf("\n\n%d"  ,teste3);
@@ -73,7 +81,7 @@ void percorrendo4(int x[][4],int teste4)
     int l,c;
     for (l=0;l<4;++l){
         for(c=0;c<4;++c){
-//          printf("%d  ",x[l][c]);
+//          printf("%d  ",x[l][c]);   // QUARTA LINHA/COLUNA E GUARDA O RESULTADO
         }
         teste4 =  x[3][0] * x[3][1] * x[3][2] * x[3][3] ;
         printf("\n\n%d"  ,teste4);
@@ -81,7 +89,7 @@ void percorrendo4(int x[][4],int teste4)
     
 }
 
-void seEHholandes(int teste,int teste2,int teste3,int teste4)
+void seEHholandes(int teste,int teste2,int teste3,int teste4) // FUNÇÃO COM CONDIÇÃO PARA SER Q.H
 {
     
     if (teste == teste2 == teste3 == teste4  ){
